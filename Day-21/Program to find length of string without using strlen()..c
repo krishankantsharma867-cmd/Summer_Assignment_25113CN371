1e02a07c9799364e938aeaffba5f154df5940808
krishankantsharma867-cmd <krishankantsharma867@gmail.com>
2026-06-21 21:48:19 +0530

    Add files via upload

---
+++ Day-21/Program to find length of string without using strlen()..c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+int main(){
+    char str[100];
+    int len=0;
+
+    printf("Enter a string: ");
+    gets(str);
+
+    while(str[len]!='\0'){
+        len++;
+    }
+
+    printf("Length of string = %d",len);
+
+    return 0;
+}
